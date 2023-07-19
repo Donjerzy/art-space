2d1db3f26619572b343a3272c96bcfd12c34877b
Donald Ray <okoth46@gmail.com>
2023-07-19 11:10:37 +0300

    Art Images

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +20,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtSpace() {
+    
+    Column(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+        
+    }
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    ArtSpaceTheme {
-        Greeting("Android")
-    }
+fun ArtCard (modifier: Modifier = Modifier) {
+
 }
+
+@Composable
+fun ArtDescription(modifier: Modifier = Modifier) {
+    
+}
+
+@Composable
+fun ArtSpaceNavigation(modifier: Modifier = Modifier) {
+    
+}
+
+
+
+@Composable
+@Preview
+fun ArtSpaceApp() {
+    ArtSpace()
+}
+
+
+
